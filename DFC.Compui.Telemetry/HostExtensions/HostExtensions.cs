d58accf4586816e6f8d2b7ed3e205c87f9f625bf
Tom Tollafield <thomas.tollafield@gmail.com>
2020-06-10 10:11:00 +0100

    Add Request Trace Add Host Extensions

---
+++ DFC.Compui.Telemetry/HostExtensions/HostExtensions.cs
@@ -0,0 +1,43 @@
+﻿using DFC.Compui.Telemetry.TelemetryInitializers;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace DFC.Compui.Telemetry.HostExtensions
+{
+    public static class HostExtensions
+    {
+        public static IWebHost AddApplicationTelemetryInitializer(this IWebHost host)
+        {
+            if (host == null)
+            {
+                throw new ArgumentNullException(nameof(host));
+            }
+
+            var telemetryConfig = (TelemetryConfiguration)host.Services.GetService(typeof(TelemetryConfiguration));
+            var logger = (ILogger<ApplicationTelemetryInitializer>)host.Services.GetService(typeof(ILogger<ApplicationTelemetryInitializer>));
+            var configuration = (IConfiguration)host.Services.GetService(typeof(IConfiguration));
+            telemetryConfig.TelemetryInitializers.Add(new ApplicationTelemetryInitializer(logger, configuration));
+
+            return host;
+        }
+
+        public static IHost AddApplicationTelemetryInitializer(this IHost host)
+        {
+            if (host == null)
+            {
+                throw new ArgumentNullException(nameof(host));
+            }
+
+            var telemetryConfig = (TelemetryConfiguration)host.Services.GetService(typeof(TelemetryConfiguration));
+            var logger = (ILogger<ApplicationTelemetryInitializer>)host.Services.GetService(typeof(ILogger<ApplicationTelemetryInitializer>));
+            var configuration = (IConfiguration)host.Services.GetService(typeof(IConfiguration));
+            telemetryConfig.TelemetryInitializers.Add(new ApplicationTelemetryInitializer(logger, configuration));
+
+            return host;
+        }
+    }
+}
---
+++ DFC.Compui.Telemetry/Models/RequestTrace.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DFC.Compui.Telemetry.Models
+{
+    public class RequestTrace
+    {
+        public string? TraceId { get; private set; }
+
+        public string? ParentId { get; private set; }
+
+        public void AddTraceId(string traceId)
+        {
+            if (string.IsNullOrWhiteSpace(traceId))
+            {
+                throw new ArgumentException($"{nameof(TraceId)} cannot be null");
+            }
+
+            TraceId = traceId;
+        }
+
+        public void AddParentId(string parentId)
+        {
+            if (string.IsNullOrWhiteSpace(parentId))
+            {
+                throw new ArgumentException($"{nameof(TraceId)} cannot be null");
+            }
+
+            ParentId = parentId;
+        }
+    }
+}
