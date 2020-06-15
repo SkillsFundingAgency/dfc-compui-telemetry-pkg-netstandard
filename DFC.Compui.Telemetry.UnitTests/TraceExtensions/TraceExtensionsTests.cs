26d3a0e9a8e8e5074c32004ad2f17cf07697cb2a
Tom Tollafield <thomas.tollafield@gmail.com>
2020-06-15 08:58:41 +0100

    - Add Trace Extension

---
+++ DFC.Compui.Telemetry.UnitTests/TraceExtensions/TraceExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using DFC.Compui.Telemetry.Models;
+using DFC.Compui.Telemetry.TraceExtensions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Xunit;
+
+namespace DFC.Compui.Telemetry.UnitTests.TraceExtensions
+{
+    public class TraceExtensionsTests
+    {
+        [Fact]
+        public void DoSomething()
+        {
+            // Arrange
+            var model = new RequestTrace();
+
+            // Act
+            var activity = new Activity("TestOperation").Start();
+            model.AddTraceInformation();
+
+            // Assert
+            Assert.NotNull(model.TraceId);
+            Assert.NotNull(model.ParentId);
+        }
+    }
+}
