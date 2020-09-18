bd353bbd1ac644643da801924414e8d83b01489e
Tom Tollafield <thomas.tollafield@gmail.com>
2020-09-18 10:57:21 +0100

    - Add request trace interface

---
+++ DFC.Compui.Telemetry/Interface/IRequestTrace.cs
@@ -0,0 +1,9 @@
+﻿namespace DFC.Compui.Telemetry.Interface
+{
+    public interface IRequestTrace
+    {
+        public string? TraceId { get; }
+
+        public string? ParentId { get; }
+    }
+}
